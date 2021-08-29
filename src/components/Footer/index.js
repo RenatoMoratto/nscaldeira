99cf5512b5ce4d7c47395f3a451a2fd64c429b55
Renato Willyan Moratto Filho <80411445+RenatoMoratto@users.noreply.github.com>
2021-08-29 03:52:32 -0300

    Create Footer Component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import style from './Footer.module.css';
+
+function Footer(props) {
+    return (
+        <footer className={style.footer}>
+            <p>
+                Feito com <i className="fa fa-heart" aria-hidden="true"></i> por <a
+                    href="https://github.com/RenatoMoratto"
+                    target="_blank"
+                    rel="noopener noreferrer">Renato Willyan</a>
+            </p>
+        </footer>)
+}
+export default Footer;
