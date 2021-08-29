a4dff1560a3bb92d0d74afcce54746fefd837659
Renato Willyan Moratto Filho <80411445+RenatoMoratto@users.noreply.github.com>
2021-08-29 03:53:13 -0300

    Create Nath.js

---
+++ pages/Nath.js
@@ -0,0 +1,14 @@
+import Head from "../src/components/Head";
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+
+export default function Nath(props) {
+    return (<>
+        <Head />
+        <Header />
+        <main>
+
+        </main>
+        <Footer />
+    </>)
+}
