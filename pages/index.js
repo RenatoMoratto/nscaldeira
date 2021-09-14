b9e42519fc6b7f9781a84721a7c05a2c27487951
Renato Willyan Moratto Filho <renatowmoratto@gmail.com>
2021-09-13 23:55:04 -0300

    feat: create Localizacao component

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import HomeButton from "../src/components/HomeButton";
 import CardGrid from "../src/components/CardGrid";
 import Contato from "../src/components/Contato";
+import Localizacao from "../src/components/Localizacao";
 
 export default function Home(props) {
     const [scrolled, setScrolled] = useState(false);
@@ -42,13 +43,7 @@ export default function Home(props) {
             </Section>
 
             <Section title="Localização" id="localizacao">
-                <p>R. Senador Souza Naves, 282</p>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2590.875781508695!2d-51.16080838118508!3d-23.313884053882923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94eb436096d52ea9%3A0x3817c2184091835!2sCondom%C3%ADnio%20Centro%20Empresarial%20Col%C3%BAmbia!5e0!3m2!1spt-BR!2sbr!4v1629345911742!5m2!1spt-BR!2sbr"
-                    width="100%"
-                    height="200"
-                    loading="lazy"
-                ></iframe>
+                <Localizacao />
             </Section>
         </main>
         <Footer />
---
+++ src/components/Localizacao/index.js
@@ -0,0 +1,18 @@
+import style from './Localizacao.module.css';
+
+export default function Localizacao() {
+    return (
+        <>
+            <div className={style.endereco}>
+                <p>R. Senador Souza Naves, 282, Sala 702 - Centro</p>
+                <p>Londrina - PR, 86010-160</p>
+            </div>
+            <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2590.875781508695!2d-51.16080838118508!3d-23.313884053882923!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94eb436096d52ea9%3A0x3817c2184091835!2sCondom%C3%ADnio%20Centro%20Empresarial%20Col%C3%BAmbia!5e0!3m2!1spt-BR!2sbr!4v1629345911742!5m2!1spt-BR!2sbr"
+                width="100%"
+                height="200"
+                loading="lazy"
+            />
+        </>
+    )
+}
